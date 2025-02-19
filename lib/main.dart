2b0831d0a60ef646fb91c9b25ef05d596be2da9e
Samuel Francisco Silva Pires <124320943+sensweettsan@users.noreply.github.com>
2025-02-19 15:59:20 -0300

    at

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,30 @@
 import 'package:app_gerenciamento_de_tarefas/firebase_options.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'presentation/pages/home_page.dart';
+import 'package:firebase_core/firebase_core.dart'; // Importe o Firebase Core
+import 'presentation/pages/home_page.dart'; // Importe a HomePage do catálogo de livros
 
 void main() async {
-  WidgetsFlutterBinding
-      .ensureInitialized(); //comando para fazer o filebase funcionar em ANDROID
+  WidgetsFlutterBinding.ensureInitialized(); // Garante que o Flutter esteja inicializado
   await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
+    options: DefaultFirebaseOptions.currentPlatform, // Inicializa o Firebase
   );
-  runApp(const MyApp());
+  runApp(const MyApp()); // Inicia o aplicativo
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // Este widget é a raiz do aplicativo.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Gerenciador de Tarefas',
-      debugShowCheckedModeBanner: false,
+      title: 'Catálogo de Livros', // Título do aplicativo
+      debugShowCheckedModeBanner: false, // Remove o banner de debug
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal), // Tema do aplicativo
+        useMaterial3: true, // Usa Material 3
       ),
-      home: const HomePage(),
+      home: const HomePage(), // Define a HomePage como a página inicial
     );
   }
 }
